ce566a0a9312708274a59afc954a7f2dc05adf27
TopchagaM <ajdintop001@gmail.com>
2024-03-23 15:36:28 +0100

    Added buttons for choices and added score counter

--- RPS.js
+++ RPS.js
@@ -1,36 +1,61 @@
+let winPlayer = 0;
+let winPC = 0;
+let p;
+
 //setting random pc choice -------------------------------------------------
-function getComputerChoice(){
+function getComputerChoice()
+{
     const pcChoice = ["Rock", "Paper", "Scissors"];
     let randChoice = Math.floor(Math.random()*3);
     console.log(pcChoice[randChoice])
     return pcChoice[randChoice];
 }
 
-
 // checking who won --------------------------------------------------------
-function playRound(p, c){
-    p = p.toLocaleLowerCase();
+function playRound(choice)
+{
+    getComputerChoice();
+    c = getComputerChoice();
     c = c.toLocaleLowerCase();
+    p = choice;
+    p = p.toLocaleLowerCase();
 
-    if (p == 'rock' && c == 'rock' || p == 'paper' && c == 'paper' || p == 'scissors' && rock == 'scissors')
-        return "Draw!";
+    if (p == c)
+    {
+        roundResoult.textContent = "Draw!"
+        results.appendChild(roundResoult)
 
-    else if (p == 'rock' && c == 'scissors' || p == 'scissors' && c == 'paper' || p == 'paper' && c == 'scissors')
-        return 'You win!';
-
-    // if we only put else it would count typos from players
-    else if (p == 'rock' && c == 'paper' || p == 'scissors' && c == 'rock' || p == 'paper' && c == 'scissors')
-        return "You loose!"
-}
-
-function game(){
-    for(let i = 0; i < 5; i++){
-        let p = prompt("Enter your choice ");
-        let c = getComputerChoice();
-        alert("Computer choice " + c);
-        console.log(playRound(p, c))
     }
+
+    else if (p == 'rock' && c == 'scissors' || p == 'scissors' && c == 'paper' || p == 'paper' && c == 'rock')
+    {
+        roundResoult.textContent = "You WIN!"
+        results.appendChild(roundResoult)
+        winPlayer++;
+    }
+
+// if we only put else it would count typos from players
+    else
+    {
+        roundResoult.textContent = "You LOSE!"
+        results.appendChild(roundResoult)
+        winPC++;
+    }
+    
+    if (winPlayer >= 5)
+    {
+        alert("You won the game!!!")
+        winPlayer = 0;
+        winPC = 0;
+    }
+
+    if (winPC >= 5)
+    {
+        alert("You lost the game!!!")
+        winPlayer = 0;
+        winPC = 0;
+    }
+
+    document.getElementById("playerScore").innerHTML = winPlayer;
+    document.getElementById("PCScore").innerHTML = winPlayer;
 }
-
-game();
-
